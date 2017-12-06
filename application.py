8acd7bb4c1bfeb339a84c9c8824d6cfb30b0a0ad
Emma Humphrey <emmahumphrey@college.harvard.edu>
2017-12-06 23:57:04 +0000

    trying to fix login issue still

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required
 
 # Configure application
 app = Flask(__name__)
